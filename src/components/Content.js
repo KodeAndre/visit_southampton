8044e13e97e0e1f7e8d8cf27702c642661435743
anarchytrex <anarchytrex@hotmail.com>
2021-08-01 01:01:04 +0200

    v0.1.6

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,13 +5,28 @@ import ReactDOM from 'react-dom'
 import Style from "../style.css"
 import demo from "../media/demo.mp4"
 
+import Card from "./Card"
+
 export default function Content() {
 
     return (
         <div>
-            <video autoPlay muted loop width="100%" height="auto" poster="https://dc-southampton.s3.amazonaws.com/videos/southampton-hero-video-poster.jpg?mtime=20190926112353&focal=none">
-                <source src={demo} type="video/mp4"/>
-            </video>
+            <div>
+                <video autoPlay muted loop width="100%" height="auto" poster="https://dc-southampton.s3.amazonaws.com/videos/southampton-hero-video-poster.jpg?mtime=20190926112353&focal=none">
+                    <source src={demo} type="video/mp4"/>
+                </video>
+            </div>
+
+            <div>
+                <span className="material-icons" id="arrowdown">
+                south
+                </span>
+            </div>
+
+            <div>
+                <Card />
+            </div>
+            
         </div>
     )
 }
