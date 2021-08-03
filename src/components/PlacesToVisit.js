981ffc5b13ed6a5347ffbc21d9e29c40da6ce30b
anarchytrex <anarchytrex@hotmail.com>
2021-08-03 19:25:28 +0200

    v Final because no interest

--- src/components/PlacesToVisit.js
+++ src/components/PlacesToVisit.js
@@ -4,11 +4,14 @@ import ReactDOM from 'react-dom'
 // eslint-disable-next-line
 import Style from "../style.css"
 
+import Card from "./Card"
+
 export default function Places() {
 
     return (
         <div id="placesContent">
-            <h2>places here</h2>
+            <h2>Places and Events to visit and try out!</h2>
+            <Card />
         </div>
     )
 }
