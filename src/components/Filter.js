5836bc6195b25c0b50ed3faeb932b1f894cc5447
anarchytrex <anarchytrex@hotmail.com>
2021-08-18 09:42:20 +0200

    v0.3.7

---
+++ src/components/Filter.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+// eslint-disable-next-line
+import ReactDOM from 'react-dom'
+// eslint-disable-next-line
+import Style from "../style.css"
+
+export default function Filter() {
+
+    const [rest, setRest] = useState(0);
+    const [eve, setEve] = useState(0);
+
+    function handleChange1(e) {
+        setRest({value: e.target.value})
+        console.log("User filtered by: " + e.target.value)
+    }
+
+    function handleChange2(e) {
+        setEve({value: e.target.value})
+        console.log("User filtered by: " + e.target.value)
+    }
+
+    return (
+        <div>
+            <label htmlFor="restFilter">Food type: </label>
+
+            <select name="restFilter" id="restFilter" defaultValue="default" onChange={handleChange1}>
+            
+                <option value="default" disabled>Choose One</option>
+                <optgroup label="Restaurants">
+                    <option value="Dolly Dimples">Dolly Dimples</option>
+                    <option value="Peppes Pizza">Peppes Pizza</option>
+                    <option value="Egon">Egon</option>
+                </optgroup>
+
+                <optgroup label="Fast Foods">
+                    <option value="McDonalds">McDonalds</option>
+                    <option value="Dominos">Dominos</option>
+                    <option value="KFC">KFC</option>
+                </optgroup>
+
+            </select>
+
+            <label htmlFor="eventFilter">Event type: </label>
+
+            <select name="eventFilter" id="eventFilter" defaultValue="default" onChange={handleChange2}>
+
+                <option value="default" disabled>Choose One</option>
+
+                    <option value="Fiesta">Fiesta</option>
+                    <option value="Party">Party</option>
+                    <option value="LAN">LAN</option>
+
+                </select>
+
+            <p>Filters applied: <b>{rest.value}</b>, <b>{eve.value}</b></p>
+        </div>
+    )
+}
