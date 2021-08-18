71ef3aa1d0c26a247d090d54e2159feb8afbfc0f
anarchytrex <anarchytrex@hotmail.com>
2021-08-18 13:47:51 +0200

    v0.4.1

--- src/components/PlacesToVisit.js
+++ src/components/PlacesToVisit.js
@@ -12,14 +12,12 @@ export default function Places() {
         <div id="placesContent">
             <h2>Places and Events to visit and try out!</h2>
             <details>
-
                 <summary>
                     Click for list of filters
                 </summary>
-
                 <Filter />
-
             </details>
+            <p>proof that the filter isnt pushing content</p>
         </div>
     )
 }
